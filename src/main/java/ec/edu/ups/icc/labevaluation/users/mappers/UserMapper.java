a947a75091888014af759c9c43def3a301d0523e
CinthyLu <cramonm1@est.ups.edu.ec>
2026-07-23 09:19:17 -0500

    r2 usuarios

--- src/main/java/ec/edu/ups/icc/labevaluation/users/mappers/UserMapper.java
+++ src/main/java/ec/edu/ups/icc/labevaluation/users/mappers/UserMapper.java
@@ -1,9 +1,23 @@
 package ec.edu.ups.icc.labevaluation.users.mappers;
+import java.util.stream.Collectors;
+
 import ec.edu.ups.icc.labevaluation.users.dtos.UserResponseDto;
 import ec.edu.ups.icc.labevaluation.users.entities.UserEntity;
 public final class UserMapper {
+
     private UserMapper() {}
+    // # R2: Mapear la entidad de usuario al nuevo DTO con sus roles mapeados a String
     public static UserResponseDto toResponse(UserEntity entity){
-        return new UserResponseDto(entity.getId(), entity.getFullName(), entity.getEmail());
+        if (entity == null) return null;
+        return new UserResponseDto(
+            entity.getId(),
+            entity.getFullName(),
+            entity.getEmail(),
+            entity.getAge(),
+            entity.isActive(),
+            entity.getRoles().stream()
+                .map(role -> role.getName().name())
+                .collect(Collectors.toSet())
+        );
     }
 }
